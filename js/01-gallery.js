8f6786786d6174bf42052efb66490e62c247e7e1
PavloF23 <107143084+PavloF23@users.noreply.github.com>
2022-10-19 23:22:36 +0300

    191020222322

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,41 +1,57 @@
-// Створення і рендер розмітки на підставі масиву даних galleryItems і наданого шаблону елемента галереї.
-// Реалізація делегування на div.gallery і отримання url великого зображення.
-// Підключення скрипту і стилів бібліотеки модального вікна basicLightbox. Використовуй CDN сервіс jsdelivr і додай у проект посилання на мініфіковані (.min) файли бібліотеки.
-// Відкриття модального вікна по кліку на елементі галереї. Для цього ознайомся з документацією і прикладами.
-// Заміна значення атрибута src елемента <img> в модальному вікні перед відкриттям. Використовуй готову розмітку модального вікна із зображенням з прикладів бібліотеки basicLightbox.
+// 1.Створення і рендер розмітки на підставі масиву даних galleryItems і наданого шаблону елемента галереї.
+// 2.Реалізація делегування на div.gallery і отримання url великого зображення.
+// 3.Підключення скрипту і стилів бібліотеки модального вікна basicLightbox. Використовуй CDN сервіс jsdelivr і додай у проект посилання на мініфіковані (.min) файли бібліотеки.
+// 4.Відкриття модального вікна по кліку на елементі галереї. Для цього ознайомся з документацією і прикладами.
+// 5.Заміна значення атрибута src елемента <img> в модальному вікні перед відкриттям. Використовуй готову розмітку модального вікна із зображенням з прикладів бібліотеки basicLightbox.
 
 import { galleryItems } from './gallery-items.js';
 
 // Change code below this line
+
+// console.log(galleryItems);
+
 // Створемо галарею
-
-console.log(galleryItems);
-
-const showSlide = document.querySelectorAll('.gallery__image');
-
 const createGalleryItem = ({preview, original, description}) =>
 `<div class="gallery__item">
     <a class="gallery__link" href="${original}">
         <img class="gallery__image" src="${preview}" alt="${description}" data-source="${original}" />
     </a>
 </div>`;
-
 const galleryMarkup = galleryItems.reduce(
     (acc, item) => acc + createGalleryItem(item),
     ""
 );
 
-const galleryList = document.querySelector(".gallery");
-galleryList.insertAdjacentHTML("afterbegin", galleryMarkup);
+const galleryList = document.querySelector(".gallery");       // посилання на DOM
+galleryList.insertAdjacentHTML("afterbegin", galleryMarkup);  // створення HTML розмітки?
 
 
-//отримання url великого зображення
-document.addEventListener('click', event => {
-   const type = event.target.dataset.source
-   const instance = basicLightbox.create(`
-   <img src="${event.target.dataset.source}">
-`)
-    // console.log(event.target.dataset);
+
+galleryList.addEventListener('click', onGalleryItemClick);   //слухач скліка для відкриття модалки
+
+// фунція  для відкритя модального вікна
+function onGalleryItemClick(event) { 
+    event.preventDefault();
+    if (event.target.nodeName !== 'IMG') {
+        return;
+    } 
+    
+    const urlOfLargeImage = event.target.dataset.source;  // адреса зображення (data-source="${original})
+    
+    // сещсоння бібліотека basicLightbox, рендер модального вікна
+    const instance = basicLightbox.create(`
+    <img src="${urlOfLargeImage}" width="800" height="600">
+    `)
+
     instance.show()
-})
+
+galleryList.addEventListener('keydown', event => {  // слухач ESC для закриття модалки
+        if (event.code === 'Escape') 
+            instance.close()
+        })
+
+}
+
+
+    
 
